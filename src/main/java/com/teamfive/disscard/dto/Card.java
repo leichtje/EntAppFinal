cb72a8428ef06f63129c74c7b23ab9fdc0048b28
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2024-10-01 16:38:04 -0400

    Added interface for card service

---
+++ src/main/java/com/teamfive/disscard/dto/Card.java
@@ -0,0 +1,4 @@
+package com.teamfive.disscard.dto;
+
+public class Card {
+}
---
+++ src/main/java/com/teamfive/disscard/service/ICardService.java
@@ -0,0 +1,33 @@
+package com.teamfive.disscard.service;
+
+import com.teamfive.disscard.dto.Card;
+
+import java.util.List;
+
+public interface ICardService {
+    /**
+     * Gets the card with the specified ID.
+     * @param id The id of the card.
+     * @return The card that matches the ID, or null if nothing is found
+     */
+    Card getById(int id);
+
+    /**
+     * Gets every card that exists.
+     * @return A list containing every card recorded.
+     */
+    List<Card> getAll();
+
+    /**
+     * Gets cards based on if the keyword matches their name.
+     * @param keyword The keyword you want to use for the search.
+     * @return A list containing any card that has a name matching or partially matching the given keyword.
+     */
+    List<Card> searchByName(String keyword);
+
+    /**
+     * Saves the given card info in storage.
+     * @param card The card you wish to record.
+     */
+    void save(Card card);
+}
