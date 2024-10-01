a6e961415b086474111c695edc452402ff3f9568
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2024-10-01 10:37:20 -0400

    Added Spring Boot template

---
+++ src/main/java/com/teamfive/disscard/DissCardApplication.java
@@ -0,0 +1,13 @@
+package com.teamfive.disscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DissCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamfive/disscard/DissCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamfive.disscard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
