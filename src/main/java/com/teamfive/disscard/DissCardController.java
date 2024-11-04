62c92337c3e43c9eedab2e04f5757c3e414f6470
Aaron George <georgak@mail.uc.edu>
2024-11-03 22:49:11 -0500

    Code review changes

--- src/main/java/com/teamfive/disscard/DissCardController.java
+++ src/main/java/com/teamfive/disscard/DissCardController.java
@@ -1,4 +1,5 @@
 package com.teamfive.disscard;
+
 import com.teamfive.disscard.dto.Card;
 import com.teamfive.disscard.service.ICardService;
 
@@ -14,98 +15,101 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 /**
- * Controller class handling web ui and endpoints for the DissCard application
+ * Controller class handling web UI and endpoints for the DissCard application.
  * <h2>Front End</h2>
  * <p>
- *     Contains endpoints to generate dynamic HTML pages using Thymeleaf
+ *     Contains endpoints to generate dynamic HTML pages using Thymeleaf.
  * </p>
- *
  * <h2>Back End</h2>
  * <p>
  *     Contains endpoints that handle CRUD operations to the database through
- *     the card service
+ *     the card service.
  * </p>
- * @author Danny Murray
  */
 @Controller
 public class DissCardController {
 
-    // ===== Fields =====
+    private static final Logger log = LoggerFactory.getLogger(DissCardController.class);
+
+    private final ICardService cardService;
 
     @Autowired
-    ICardService cardService;
-
-    Logger log = LoggerFactory.getLogger(this.getClass());
+    public DissCardController(ICardService cardService) {
+        this.cardService = cardService;
+    }
 
     // ===== Front-end endpoints =====
 
     /**
-     * Endpoint that generates the home page of the DissCard application's web UI
-     * @param model Object used to pass data to the thymeleaf html template
-     * @return DissCard home page
+     * Endpoint that generates the home page of the DissCard application's web UI.
+     * @param model Object used to pass data to the Thymeleaf HTML template.
+     * @return DissCard home page.
      */
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index(Model model) {
         // Add data to model for use in view here
-
-        // Return name of html template to
         return "home";
     }
 
     // ===== Back-end endpoints =====
 
     /**
-     * Fetches a list of all cards registered with DissCard
-     * @return A list of JSON objects representing cards
+     * Fetches a list of all cards registered with DissCard.
+     * @return A list of JSON objects representing cards.
      */
     @GetMapping("/card/list")
     @ResponseBody
     public List<Card> fetchAllCards() {
+        log.info("Fetching all cards.");
         return cardService.getAll();
     }
 
     /**
-     * Fetches a card's info based on its ID
-     * @param id id of a card registered with DissCard
-     * @return A JSON object representing a card
+     * Fetches a card's info based on its ID.
+     * @param id ID of a card registered with DissCard.
+     * @return A ResponseEntity containing a JSON object representing a card.
      */
     @GetMapping("/card/info/{id}")
     public ResponseEntity<Card> fetchCardById(@PathVariable("id") int id) {
+        log.info("Fetching card with ID: {}", id);
         Card fetchedCard = cardService.getById(id);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(fetchedCard, headers, HttpStatus.OK);
+        return buildResponse(fetchedCard);
     }
 
     /**
-     * Searches DissCard's systems for a list of cards that match the specified keyword
-     * @param keyword String used to filter cards
-     * @return A list of card JSON objects that match the specified keyword
+     * Searches DissCard's systems for a list of cards that match the specified keyword.
+     * @param keyword String used to filter cards.
+     * @return A ResponseEntity containing a list of card JSON objects that match the specified keyword.
      */
     @GetMapping("/card/search/{keyword}")
     public ResponseEntity<List<Card>> fetchCardsByKeyword(@PathVariable("keyword") String keyword) {
+        log.info("Searching for cards with keyword: {}", keyword);
         List<Card> fetchedCards = cardService.searchByName(keyword);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return new ResponseEntity<>(fetchedCards, headers, HttpStatus.OK);
+        return buildResponse(fetchedCards);
     }
 
     /**
-     * Adds a card to DissCard's systems
-     * @param card Card to be added
-     * @return A JSON object representing the card that was added
+     * Adds a card to DissCard's systems.
+     * @param card Card to be added.
+     * @return A ResponseEntity containing a JSON object representing the card that was added.
      */
     @PostMapping(value="/card/add", consumes="application/json", produces="application/json")
     public ResponseEntity<Card> addCard(@RequestBody Card card) {
-        Card newCard = null;
-        HttpHeaders headers = new HttpHeaders();
+        log.info("Adding new card: {}", card);
         try {
-            newCard = cardService.save(card);
+            Card newCard = cardService.save(card);
+            return buildResponse(newCard);
+        } catch (Exception e) {
+            log.error("Error adding card: {}", e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        catch (Exception e) {
-            log.error(e.getMessage());
-            return new ResponseEntity<>(headers, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-        return new ResponseEntity<>(newCard, headers, HttpStatus.OK);
+    }
+
+    // Utility method to build a standardized ResponseEntity
+    private <T> ResponseEntity<T> buildResponse(T body) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
     }
 }
+
