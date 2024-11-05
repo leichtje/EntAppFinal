248c6a77ed9a98de3752fed6879a662f6e54bcc5
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2024-11-05 16:58:40 -0500

    Added stub for CardDAO

---
+++ src/main/java/com/teamfive/disscard/dao/CardDAOStub.java
@@ -0,0 +1,56 @@
+package com.teamfive.disscard.dao;
+
+import com.teamfive.disscard.dto.Card;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CardDAOStub implements ICardDAO {
+
+    // Represents all the cards stored in the database
+    Map<Integer, Card> cardDataStore = new HashMap<>();
+
+    @Override
+    public Card save(Card card) throws Exception {
+
+        int cardId = card.getId();
+
+        if (cardDataStore.containsKey(cardId)) {
+            throw new Exception("Card already exists.");
+        } else {
+            cardDataStore.put(cardId, card);
+            return card;
+        }
+    }
+
+    @Override
+    public List<Card> getAll() {
+        return new ArrayList<>(cardDataStore.values());
+    }
+
+    @Override
+    public Card get(int id) {
+        return cardDataStore.get(id);
+    }
+
+    @Override
+    public List<Card> getByName(String keyword) {
+
+        ArrayList<Card> foundCards = new ArrayList<>();
+
+        cardDataStore.forEach((id, card) -> {
+            if (card.cardName.contains(keyword)) {
+                foundCards.add(card);
+            }
+        });
+
+        return foundCards;
+    }
+
+    @Override
+    public void delete(int id) {
+        cardDataStore.remove(id);
+    }
+}
