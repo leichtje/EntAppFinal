1ef97150de7b9b93bf1e1db09e7b50a45a96fca2
DragonGuyMan <108588518+DragonGuyMan@users.noreply.github.com>
2024-11-22 16:05:26 -0500

    Made DAO return empty card is one isn't found with given id

--- src/main/java/com/teamfive/disscard/dao/CardDAO.java
+++ src/main/java/com/teamfive/disscard/dao/CardDAO.java
@@ -1,16 +1,20 @@
 package com.teamfive.disscard.dao;
 
 import com.teamfive.disscard.dto.Card;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CardDAO implements ICardDAO {
 
-    CardRepository cardRepository;
+    private CardRepository cardRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CardDAO.class);
 
     public CardDAO() {}
 
@@ -28,9 +32,8 @@ public class CardDAO implements ICardDAO {
     public List<Card> getAll() {
 
         ArrayList<Card> cards = new ArrayList<>();
-        Iterable<Card> allCards = cardRepository.findAll();
 
-        for (Card card : allCards) {
+        for (Card card : cardRepository.findAll()) {
             cards.add(card);
         }
         return cards;
@@ -38,14 +41,20 @@ public class CardDAO implements ICardDAO {
 
     @Override
     public Card findById(int id) {
-        return cardRepository.findById(id).orElseThrow();
+        Optional<Card> card = cardRepository.findById(id);
+
+        if (card.isPresent()) {
+            return card.get();
+        } else {
+            logger.warn("Card with Id {} not found by card repository. Returning card with default values.", id);
+            return new Card();
+        }
     }
 
     @Override
     public List<Card> getByName(String keyword) {
 
         ArrayList<Card> cards = new ArrayList<>();
-        Iterable<Card> allCards = cardRepository.findAll();
 
         for (Card card : cardRepository.findAll()) {
 
