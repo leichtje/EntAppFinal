c26b34c7d8fca5fef743f8a7650b165e997077e0
Danny Murray <murraydp@mail.uc.edu>
2024-11-28 12:43:01 -0500

    Added pokemon API values to application.properties file

--- src/main/java/com/teamfive/disscard/dao/PokemonRetrofitClientInstance.java
+++ src/main/java/com/teamfive/disscard/dao/PokemonRetrofitClientInstance.java
@@ -1,11 +1,13 @@
 package com.teamfive.disscard.dao;
 
+import org.springframework.beans.factory.annotation.Value;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PokemonRetrofitClientInstance {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://api.pokemontcg.io/v2/";
+    @Value("${pokemon-api.base-url}")
+    private static String BASE_URL;
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
